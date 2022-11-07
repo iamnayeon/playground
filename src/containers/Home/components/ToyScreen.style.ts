5de66060860edda7c8ca54a9362965d9fd6fcecb
iamnayeon <nana.nayeonk@gmail.com>
2022-11-07 21:14:24 +0900

    add html screen section

---
+++ src/containers/Home/components/ToyScreen.style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+   width: 1024px; 
+   height: 670px;
+   border: none;
+   border-radius: 1,
+   background-color: black;
+`;
---
+++ src/containers/Home/components/ToyScreen.tsx
@@ -0,0 +1,36 @@
+import { useRef, useEffect } from "react";
+import { Html } from "@react-three/drei";
+import Me from "./Me";
+import * as S from "./ToyScreen.style";
+export default ({ object }: { object: any }) => {
+  const htmlRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (htmlRef.current) {
+      //events for fixing problem of disabled scrolling on the html section
+      const htmlWrapperByDrei = htmlRef.current.parentElement as HTMLDivElement;
+      const mouseMoveHandler = () => (htmlWrapperByDrei.style.pointerEvents = "auto");
+      const wheelHandler = () => (htmlWrapperByDrei.style.pointerEvents = "none");
+
+      htmlWrapperByDrei.addEventListener("mousemove", mouseMoveHandler);
+      htmlWrapperByDrei.addEventListener("wheel", wheelHandler);
+
+      return () => {
+        htmlWrapperByDrei.removeEventListener("mousemove", mouseMoveHandler);
+        htmlWrapperByDrei.removeEventListener("wheel", wheelHandler);
+      };
+    }
+  }, []);
+
+  return (
+    <group>
+      <primitive object={object}>
+        <Html ref={htmlRef} transform occlude distanceFactor={0.72} position={[0, 0, 0.21]}>
+          <S.Wrapper>
+            <Me />
+          </S.Wrapper>
+        </Html>
+      </primitive>
+    </group>
+  );
+};
