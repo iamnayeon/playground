11204b3d1c0cf77176258adf7935b573e528f477
iamnayeon <nana.nayeonk@gmail.com>
2022-11-07 21:18:44 +0900

    add header section

---
+++ src/containers/Home/components/Header.style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  position: absolute;
+  padding: 1.5rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  font-family: lexendDeca;
+  z-index: 1;
+  width: 100%;
+`;
+export const Title = styled.h1`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
---
+++ src/containers/Home/components/Header.tsx
@@ -0,0 +1,22 @@
+import { useContainer } from "unstated-next";
+import * as S from "./Header.style";
+import ThreeContext from "../context/ThreeContext";
+
+export default () => {
+  const { scroll, three } = useContainer(ThreeContext);
+
+  return (
+    <S.Header>
+      <S.Title
+        onClick={() => {
+          if (!scroll || !three) return;
+          scroll.el.scrollTop = 0;
+          three.scene.rotation.y = 0;
+          three.scene.rotation.x = 0;
+        }}
+      >
+        Nayeon Kim
+      </S.Title>
+    </S.Header>
+  );
+};
