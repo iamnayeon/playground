0df77e405286f5d881b03f292d34fb9387830435
iamnayeon <nana.nayeonk@gmail.com>
2022-12-28 12:33:33 +0900

    chore: add webpack configuration & declaration for import glb

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/images",
+          outputPath: "static/images/",
+        },
+      },
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
